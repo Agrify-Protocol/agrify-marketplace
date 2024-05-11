9e13ec678693e8f16bdde0ec9cbc0907896ff3b5
Progress Aienobe <paienobe@gmail.com>
2024-05-11 11:14:12 +0100

    project-details

--- src/components/SectionParent/SectionParent.tsx
+++ src/components/SectionParent/SectionParent.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import { Box } from "@chakra-ui/react";
 import React from "react";
 import ProjectOverview from "../ProjectOverview/ProjectOverview";
+import { useProjectPageContext } from "@/context/ProjectsPageContext/ProjectsPageContext";
+import { sections } from "@/context/ProjectsPageContext/constants";
+import ProjectDetails from "../ProjectDetails/ProjectDetails";
 
 const SectionParent = () => {
+  const { currentSection } = useProjectPageContext();
   return (
     <Box>
-      <ProjectOverview />
+      {currentSection === sections[0] && <ProjectOverview />}
+      {currentSection === sections[1] && <ProjectDetails />}
     </Box>
   );
 };
