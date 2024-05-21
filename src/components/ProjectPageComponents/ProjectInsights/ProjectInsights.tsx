9b9baf390084c1148c04406e8dd04e017719553d
Progress Aienobe <paienobe@gmail.com>
2024-05-21 22:15:13 +0100

    chart_setup

--- src/components/ProjectPageComponents/ProjectInsights/ProjectInsights.tsx
+++ src/components/ProjectPageComponents/ProjectInsights/ProjectInsights.tsx
@@ -1,8 +1,30 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+"use client";
+
+import { Box, Text } from "@chakra-ui/react";
+import InsightBarChart from "../InsightBarChart/InsightBarChart";
 
 const ProjectInsights = () => {
-  return <Box minH={"30rem"}>Insights</Box>;
+  return (
+    <Box minH={"30rem"}>
+      <Box
+        p={"2.5rem"}
+        w={"58.5rem"}
+        bgColor={"white"}
+        my={"1.5rem"}
+        borderRadius={"0.5rem"}
+      >
+        <Text
+          fontWeight={450}
+          fontSize={"1.5rem"}
+          color={"black"}
+          mb={"2.5rem"}
+        >
+          Land Primary Production Over Time
+        </Text>
+        <InsightBarChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default ProjectInsights;
