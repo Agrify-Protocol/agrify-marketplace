70e8d994d8b1603d86036a442a79222c9afad264
Progress Aienobe <paienobe@gmail.com>
2024-06-06 16:56:18 +0100

    overview_gallery

--- src/components/ProjectPageComponents/ProjectOverviewGallery/ProjectOverviewGallery.tsx
+++ src/components/ProjectPageComponents/ProjectOverviewGallery/ProjectOverviewGallery.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import { Box, Grid, Text } from "@chakra-ui/react";
 import React from "react";
 import Project from "../Project/Project";
-import { projects } from "../ProjectsContainer/constants";
 import { projectSections } from "@/context/ProjectsPageContext/constants";
 import ViewSectionButton from "@/components/Layout/ViewSectionButton/ViewSectionButton";
+import { useProjectPageContext } from "@/context/ProjectsPageContext/ProjectsPageContext";
+import { Project as ProjectType } from "@/context/GlobalContext/types";
 
 const ProjectOverviewGallery = () => {
+  const { project } = useProjectPageContext();
+  const fauxProjects = project?.images.map((image) => {
+    return {
+      _id: image._id,
+      title: image.description,
+      tags: [],
+      coverImage: image.image,
+    } as ProjectType;
+  });
   return (
     <Box mt={"3rem"}>
       <Text fontSize={"1.125rem"} color={"black"} p={"0.714rem"} pl={0}>
@@ -16,9 +28,9 @@ const ProjectOverviewGallery = () => {
         gridTemplateColumns={"repeat(auto-fill, minmax(17.5rem, 1fr))"}
         mt={"1.5rem"}
       >
-        <Project project={projects[2]} isGalleryItem />
-        <Project project={projects[2]} isGalleryItem />
-        <Project project={projects[2]} isGalleryItem />
+        {fauxProjects?.map((project) => {
+          return <Project key={project._id} project={project} isGalleryItem />;
+        })}
       </Grid>
       <ViewSectionButton text="Go to gallery" section={projectSections[3]} />
     </Box>
