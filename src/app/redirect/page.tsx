38d3b0b413cbb9ada4be837c397ce5e7d5471fc1
chiomanwocha <chiomanwocha17@gmail.com>
2026-02-10 16:02:08 +0100

    feat: implemented sign MPToken authorization on xaman

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import PaystackRedirection from "@/components/PaystackRedirection";
 import { useSearchParams } from "next/navigation";
 import { getCarboncreditById, getProduceDetails } from "@/services/api/profile";
 import { useToast } from "@chakra-ui/react";
 import { useAuthContext } from "@/context/AuthContext/AuthContext";
 import PageLoader from "@/components/Common/PageLoader/PageLoader";
+import SignXaman from "@/components/PaymentPageComponents/SignXaman";
 
 const Confirmation = () => {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
-  const tab = searchParams.get("tab");
+  const tab = searchParams.get("tab")?.replace("%", " "); //climate art, traceable produce, sign
+  const harsh = searchParams.get("harsh");
   const toast = useToast();
   const { user } = useAuthContext();
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<Record<string, any>>({});
+  const isNotSignTab = useMemo(() => {
+    return (
+      ["traceable produce", "climate art"].includes(tab as string) || !harsh
+    );
+  }, [tab]);
 
   const getRedirectType = (status: string) => {
     switch (status) {
@@ -31,25 +38,33 @@ const Confirmation = () => {
 
   useEffect(() => {
     if (user) {
-      (tab === "traceable produce" ? getProduceDetails : getCarboncreditById)(
-        id as string,
-        toast,
-      ).then((response) => {
-        if (response) {
-          setData(response);
-          setLoading(false);
-        }
+      if (isNotSignTab) {
+        (tab === "traceable produce" ? getProduceDetails : getCarboncreditById)(
+          id as string,
+          toast,
+        )
+          .then((response) => {
+            if (response) {
+              setData(response);
+            }
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      } else {
         setLoading(false);
-      });
+      }
     }
-  }, [user, id]);
+  }, [user, id, isNotSignTab]);
 
-  return !loading && (data?.data?.status || data?.status) ? (
+  return loading ? (
+    <PageLoader />
+  ) : !isNotSignTab ? (
+    <SignXaman harsh={harsh || ""} />
+  ) : (
     <PaystackRedirection
       type={getRedirectType(data?.data?.status || data?.status)}
     />
-  ) : (
-    <PageLoader />
   );
 };
 
